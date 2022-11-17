8877acf0413ad2273b847ca73c8d43cf960afc31
Stephen Njuguna Njambi <stepehen.jambi@student.moringaschool.com>
2022-11-17 10:44:17 +0300

    Completed the lab

--- looping.rb
+++ looping.rb
@@ -1,5 +1,11 @@
 def happy_new_year
   # your code here
+  counter = 10
+  while counter > 0
+    puts counter
+    counter -= 1
+  end
+  puts "Happy New Year!"
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
@@ -17,8 +23,27 @@ end
 
 def fizzbuzz_printer
   # your code here
+  num = 1
+  while num <= 100
+    if num % 3 == 0 && num % 5 == 0
+      puts "FizzBuzz"
+    elsif num % 3 == 0
+      puts "Fizz"
+    elsif num % 5 == 0
+      puts "Buzz"
+    else
+      puts num
+    end
+    num+=1
+  end
 end
 
 def reverse_string(str)
   # your code here
+  string = str.split("")
+  rev = "".split("")
+  string.length.downto(0){
+    |n| rev << str[n]
+  }
+  return rev.join("")
 end
